dd474b13c385cd7cfa0ba91977ccffe8420c2578
fredrikblomberglarsson <fredrik.blomberg.larsson@gmail.com>
2020-09-16 12:48:13 +0200

    Added initial structure to project

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <p class="test">TEST THAT EVERYTHING WORKS</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style scoped>
+.test {
+  color: red;
+}
+</style>
